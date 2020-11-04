00b1fbc0cbbfdd7ff557d59812b411e530e8158a
Sean Hoffman <Sean.Hoffman9@T-Mobile.com>
2020-11-04 14:19:08 -0600

    Added four flags (a,s,i,n) to specify type of input for individual ip, list of ip addresses, individual subnet, list of subnets

--- ip_checker.py
+++ ip_checker.py
@@ -7,6 +7,8 @@ import ipaddress
 parser = argparse.ArgumentParser(description='Check if an IP address is in a subnet.')
 parser.add_argument('-a', '--address', metavar = '<IP Address>')
 parser.add_argument('-s', '--subnet', metavar = '<Subnet>')
+parser.add_argument('-i', '--iplist', metavar = '<IP List Input File>')
+parser.add_argument('-n', '--networklist', metavar = '<Network List Input File>')
 
 args = parser.parse_args()
 
@@ -15,21 +17,29 @@ def check_ip_against_subnet(ip, subnet):
     subnet = subnet.rstrip()
     if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(subnet):
         print(ip + " is in " + subnet)
+
+def get_help():
+    print("Please see help info for valid inputs: ip_checker.py -h")
+    sys.exit()
  
+
 if args.address and args.subnet:
     check_ip_against_subnet(args.address, args.subnet)
     sys.exit()
-else:
-    print('Checking IPs in: ' + sys.argv[1])
-    print('Checking against subnets in: ' + sys.argv[2])
 
-    ip_file = open('./ips_accessed.txt', 'r')
-    subnet_file = open('./anyconnect_subnets.txt', 'r')
+elif args.iplist and args.networklist:
+    print('Checking IPs in: ' + args.iplist)
+    print('Checking against subnets in: ' + args.networklist)
+
+    ip_file = open(str(args.iplist), 'r')
+    subnet_file = open(str(args.networklist), 'r')
 
     ips = ip_file.readlines()
     subnets = subnet_file.readlines()
-    ip_count = 0
+    ip_file.close()
+    subnet_file.close()
 
+    ip_count = 0
     for ip in ips:
         for subnet in subnets:
             check_ip_against_subnet(ip, subnet)
@@ -37,7 +47,6 @@ else:
 
     print('Checked {} IPs'.format(ip_count))
 
-# string.rstrip() removes trailing whitespace including newline characters (\n)
-# this is important because the ipaddress functinos only take numerical values and 
-# the python readlines() function includes the newline character at the end of the string
+else:
+    get_help()
 
